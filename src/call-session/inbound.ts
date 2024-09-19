66c49dbdb75c4a8d3a3b18554183232a15815303
Ivan Espinoza Hernandez <iespinoza@intekglobal.com>
2024-09-18 17:27:25 -0700

    adds protocol customization for inbound calls

--- src/call-session/inbound.ts
+++ src/call-session/inbound.ts
@@ -3,6 +3,29 @@ import { randomInt } from '../utils';
 import type Softphone from '../softphone';
 import CallSession from '.';
 
+type Protocol = { id: number, rtpmap: string, fmtp?: string };
+export const protocols: Protocol[] = [
+  // { id: 0, rtpmap: 'PCMU/8000' },
+  // { id: 8, rtpmap: 'pcma/8000' },
+  { id: 9, rtpmap: 'g722/8000' },
+  { id: 101, rtpmap: 'telephone-event/8000', fmtp: '0-15' },
+  { id: 103, rtpmap: 'telephone-event/16000', fmtp: '0-15' },
+]
+
+export function createSDPAnswer(protocols: Protocol[], client = 'rc-ssoftphone-ts') {
+  const protocolIDs = protocols.map(p=>p.id).join(' ');
+  const attributes = protocols.map(p=>`a=rtpmap:${p.id} ${p.rtpmap}`+(p.fmtp?`\na=fmtp:${p.id} ${p.fmtp}`:'')).join('\n');
+  return `
+v=0
+o=- ${randomInt()} 0 IN IP4 127.0.0.1
+s=${client}
+c=IN IP4 127.0.0.1
+t=0 0
+m=audio ${randomInt()} RTP/AVP ${protocolIDs}
+a=sendrecv
+${attributes}`.trim();
+}
+
 class InboundCallSession extends CallSession {
   public constructor(softphone: Softphone, inviteMessage: InboundMessage) {
     super(softphone, inviteMessage);
@@ -11,22 +34,23 @@ class InboundCallSession extends CallSession {
   }
 
   public async answer() {
-    const answerSDP = `
-v=0
-o=- ${randomInt()} 0 IN IP4 127.0.0.1
-s=rc-softphone-ts
-c=IN IP4 127.0.0.1
-t=0 0
-m=audio ${randomInt()} RTP/AVP 0 8 9 101 103
-a=sendrecv
-a=rtpmap:0 PCMU/8000
-a=rtpmap:8 pcma/8000
-a=rtpmap:9 g722/8000
-a=rtpmap:101 telephone-event/8000
-a=fmtp:101 0-15
-a=rtpmap:103 telephone-event/16000
-a=fmtp:103 0-15
-`.trim();
+    const answerSDP = createSDPAnswer(protocols, 'sibatel-softphone');
+//     const answerSDP = `
+// v=0
+// o=- ${randomInt()} 0 IN IP4 127.0.0.1
+// s=rc-softphone-ts
+// c=IN IP4 127.0.0.1
+// t=0 0
+// m=audio ${randomInt()} RTP/AVP 0 8 9 101 103
+// a=sendrecv
+// a=rtpmap:0 PCMU/8000
+// a=rtpmap:8 pcma/8000
+// a=rtpmap:9 g722/8000
+// a=rtpmap:101 telephone-event/8000
+// a=fmtp:101 0-15
+// a=rtpmap:103 telephone-event/16000
+// a=fmtp:103 0-15
+// `.trim();
     const newMessage = new ResponseMessage(
       this.sipMessage,
       200,
